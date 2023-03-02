a99a6b92c9040bbc94f64ff9f246a8dfae42c52a
KiliGrubi <kilian.grubert@gmx.de>
2023-03-02 14:34:51 +0100

    Finally, after all this Bullshit: it Runs!!!!!!!!!

--- src/main/java/server/SpringBootApiServer.java
+++ src/main/java/server/SpringBootApiServer.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @SpringBootApplication
 @RestController
 public class SpringBootApiServer {
