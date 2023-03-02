c64f4782a07b37e58e364ef476f229ef2356fac4
aaron-ludwig-hagen.hoeper <aaron.hoeper1@gmx.de>
2023-03-02 14:12:35 +0100

    Maven changes

--- src/main/java/server/SpringBootApiServer.java
+++ src/main/java/server/SpringBootApiServer.java
@@ -3,8 +3,10 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -17,5 +19,10 @@ public class SpringBootApiServer {
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
-}
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
