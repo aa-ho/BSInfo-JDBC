b5630e20412d656eb2c8e24f12fe10bcbd96dacb
alba.popova <albapopova-23@outlook.de>
2023-03-07 14:10:34 +0100

    Fixed Ablesebogen

--- src/main/java/frontend/Ablesebogen.java
+++ src/main/java/frontend/Ablesebogen.java
@@ -7,45 +7,47 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Ablesebogen {
 
-    public class Ablesebogen extends JFrame {
+public class Ablesebogen extends JFrame {
 
-        private final JPanel ab;
-        private final JLabel kundennummer, zaehlerart, zaehlernummer, eingebaut, zaehlerstand, kommentar, datum;
-        private final JTextField kundennummer1, zaehlerart1, zaehlernummer1, zaehlerstand1, kommentar1, datum1;
-        private final JCheckBox eingebaut1;
-        private final JButton speichernButton;
-        private final JLabel ausgabe;
+    public static void main(String[] args) {
+        new Ablesebogen();
+    }
 
-        public static void zeigeMeldung(String error) {
-            JOptionPane.showMessageDialog(null, error, "Fehler", JOptionPane.ERROR_MESSAGE);
-        }
+    private final JPanel ab;
+    private final JLabel kundennummer, zaehlerart, zaehlernummer, eingebaut, zaehlerstand, kommentar, datum;
+    private final JTextField kundennummer1, zaehlerart1, zaehlernummer1, zaehlerstand1, kommentar1, datum1;
+    private final JCheckBox eingebaut1;
+    private final JButton speichernButton;
+    private final JLabel ausgabe;
+
+    public static void zeigeMeldung(String error) {
+        JOptionPane.showMessageDialog(null, error, "Fehler", JOptionPane.ERROR_MESSAGE);
+    }
 
 
-        public <SafeDataToJSON> Ablesebogen() {
-            super("Ablesebogen");
-            final Container container = getContentPane();
+    public Ablesebogen() {
+        super("Ablesebogen");
+        final Container container = getContentPane();
 
-            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-            Date date = new Date();
-            String dateToString = dateFormat.format(date);
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = new Date();
+        String dateToString = dateFormat.format(date);
 
-            ab = new JPanel(new GridLayout(8, 2));
-            ab.setBorder(new EmptyBorder(5, 15, 5, 15));
+        ab = new JPanel(new GridLayout(8, 2));
+        ab.setBorder(new EmptyBorder(5, 15, 5, 15));
 
-            kundennummer = new JLabel("Kundennummer");
-            zaehlerart = new JLabel("Zählerart");
-            zaehlernummer = new JLabel("Zählernummer");
-            datum = new JLabel("Datum");
-            eingebaut = new JLabel("neu eingebaut");
-            zaehlerstand = new JLabel("Zählerstand");
-            kommentar = new JLabel("Kommentar");
+        kundennummer = new JLabel("Kundennummer");
+        zaehlerart = new JLabel("Zählerart");
+        zaehlernummer = new JLabel("Zählernummer");
+        datum = new JLabel("Datum");
+        eingebaut = new JLabel("neu eingebaut");
+        zaehlerstand = new JLabel("Zählerstand");
+        kommentar = new JLabel("Kommentar");
 
-            speichernButton = new JButton("Speichern");
+        speichernButton = new JButton("Speichern");
 
-            ausgabe = new JLabel("<--  Bitte Speichern", SwingConstants.CENTER/*RIGHT*/  //);
-
+        ausgabe = new JLabel("<--  Bitte Speichern", SwingConstants.CENTER);
 
 
         kundennummer1 = new JTextField();
@@ -59,9 +61,8 @@ public class Ablesebogen {
 
         speichernButton.addActionListener(e -> {
             try {
-                SafeDataToJSON safeData = new SafeDataToJSON();
-                safeData.safeData(Integer.parseInt(kundennummer1.getText()), zaehlerart1.getText(), Integer.parseInt(zaehlernummer1.getText()), dateToString, eingebaut1.isSelected(), zaehlerstand1.getText(), kommentar1.getText());
-
+                //SafeDataToJSON safeData = new SafeDataToJSON();
+                //safeData.safeData(Integer.parseInt(kundennummer1.getText()), zaehlerart1.getText(), Integer.parseInt(zaehlernummer1.getText()), dateToString, eingebaut1.isSelected(), zaehlerstand1.getText(), kommentar1.getText());
                 ausgabe.setText("Speicherung war erfolgreich");
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -69,7 +70,6 @@ public class Ablesebogen {
                 ausgabe.setText("Speicherung fehlgeschlagen!");
                 return;
             }
-
             kundennummer1.setText("");
             zaehlerart1.setText("");
             zaehlernummer1.setText("");
@@ -77,8 +77,7 @@ public class Ablesebogen {
             eingebaut1.setSelected(false);
             zaehlerstand1.setText("");
             kommentar1.setText("");
-
-  
+        });
         ab.add(kundennummer);
         ab.add(kundennummer1);
         ab.add(zaehlerart);
@@ -102,12 +101,7 @@ public class Ablesebogen {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public static void main(String[]Object args;
-            args) {
-        new Ablesebogen();
-    }
-
-    
-    
-    
 }
+    
+    
+
