6a073e92c11cfabe8740ddbce93ce45b31599bd0
alba.popova <albapopova-23@outlook.de>
2023-03-09 09:27:50 +0100

    Added Kunden.java

---
+++ src/main/java/frontend/Kunden.java
@@ -0,0 +1,17 @@
+package frontend;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Kunden extends JFrame {
+
+    public static void main(String[] args) {
+        new Kunden();
+    }
+
+    public Kunden() {
+        super("Kunden00");
+        final Container container = getContentPane();
+
+    }
+}
